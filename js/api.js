bb5cd5c274e64c369464e962cab7a5b410c286a4
Cao Minh Trí <caominhtri.business@gmail.com>
2025-07-31 22:47:09 +0700

    Cập nhật API_BASE và xóa workflow không dùng

--- js/api.js
+++ js/api.js
@@ -1,14 +1,17 @@
+
 const API_BASE = "https://script.google.com/macros/s/AKfycbzK034VDCfzxBhEpKYirPjZO1PnDTKZ7VZ1SxVysIIK_2fkCt5gK790hY0Qr-cD-S50/exec";
 
+/**
+ * Gửi POST tới backend
+ */
 async function apiPost(path, data = {}) {
-  const res = await fetch(`${API_BASE}?path=${path}&method=POST`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  if (!res.ok) throw new Error("Lỗi khi gọi API");
-  return res.json();
+  const res = await fetch(
+    `${API_BASE}?path=${path}&method=POST`, 
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    }
+  );
+  return await res.json();
 }
