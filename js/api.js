c5200bccfa93ce564f166b34602ad93dfe71b01c
Cao Minh Trí <133616254+CaoTri253@users.noreply.github.com>
2025-10-28 11:04:26 +0700

    Cập nhật Admin

--- js/api.js
+++ js/api.js
@@ -86,8 +86,14 @@ async function login(sdt, password) {
   const res = await fetch(url);
   return await res.json();
 }
+// async function register(user) {
+//   const url = `${API_BASE}?func=Register&ho_ten=${encodeURIComponent(user.ho_ten)}&sdt=${encodeURIComponent(user.sdt)}&password=${encodeURIComponent(user.password)}&ma_truong=${encodeURIComponent(user.ma_truong)}&loai_user=${encodeURIComponent(user.loai_user)}`;
+//   const res = await fetch(url);
+//   return await res.json();
+// }
 async function register(user) {
-  const url = `${API_BASE}?func=Register&ho_ten=${encodeURIComponent(user.ho_ten)}&sdt=${encodeURIComponent(user.sdt)}&password=${encodeURIComponent(user.password)}&ma_truong=${encodeURIComponent(user.ma_truong)}&loai_user=${encodeURIComponent(user.loai_user)}`;
+  // Thêm encodeURIComponent(user.lop_hoc || "") vào URL
+  const url = `${API_BASE}?func=Register&ho_ten=${encodeURIComponent(user.ho_ten)}&sdt=${encodeURIComponent(user.sdt)}&password=${encodeURIComponent(user.password)}&ma_truong=${encodeURIComponent(user.ma_truong)}&loai_user=${encodeURIComponent(user.loai_user)}&lop_hoc=${encodeURIComponent(user.lop_hoc || "")}`;
   const res = await fetch(url);
   return await res.json();
 }
